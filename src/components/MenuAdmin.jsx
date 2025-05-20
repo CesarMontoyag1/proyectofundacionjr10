8b5a06ef911b11d4983791a61e7e89c327d567e1
Isabela <imendozam@eafit.edu.co>
2025-05-20 01:45:33 -0500

    act4

--- src/components/MenuAdmin.jsx
+++ src/components/MenuAdmin.jsx
@@ -6,9 +6,13 @@ export default function MenuAdmin() {
     return (
         <>
             <NavBarWithButtons />
+            <div className={styles.horizontalLine}></div> {/* Línea horizontal por encima */}
             <div className={styles.container}>
                 <div className={styles.leftSection}></div>
-                <div className={styles.rightSection}></div>
+                <div className={styles.rightSection}>
+                    <h2 className={styles.topRightSubtitle}>Menú Administrativos</h2>
+                    <h1 className={styles.title}>¡Bienvenido!</h1>
+                </div>
             </div>
         </>
     );
