218d7f553727a553ead0e599abc5bc1ecf8c5c7b
CesarMontoyag1 <159492748+CesarMontoyag1@users.noreply.github.com>
2025-05-20 00:52:40 -0500

    s

--- src/components/MenuAdmin.jsx
+++ src/components/MenuAdmin.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from '../styles/menuAdmin.module.css'; // Importa el archivo CSS
+import styles from '../styles/menuAdmin.module.css';
 
 export default function MenuAdmin() {
     return (
-        <div>
-            <h1 style={{ color: 'white' }}>Menú Administrador</h1>
-            
+        <div className={styles.container}>
+            <h1 className={styles.title}>Menú Administrador</h1>
         </div>
     );
 }
